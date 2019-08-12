797c3895d0f798feb261548b329cb0eef8daacb5
wanghongyun <947962910@qq.com>
2019-08-12 10:19:36 +0800

    初始化

---
+++ Caculator/Caculator/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Caculator
+//
+//  Created by 王洪运 on 2019/8/12.
+//  Copyright © 2019 com.why. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
